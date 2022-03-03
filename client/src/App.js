333d8575abbfc06f8e26f340cb895d13986afc42
Leekyeongmi <starry3ones@gmail.com>
2022-03-03 12:08:31 +0900

    Resolve: #5 클라 초기환경 세팅완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Switch,
+  Route,
+  useHistory,
+  Redirect,
+  BrowserRouter,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Rollingpaper from './pages/Rollingpaper';
+import Signup from './pages/Signup';
 import axios from 'axios';
-import React, { useEffect } from 'react';
 
 function App() {
+  // 서버연결 확인코드
   useEffect(() => {
     axiosTest();
   });
   const axiosTest = () => {
     axios
-      .get('')
+      .get('http://localhost:5500')
       .then(res => console.log(res.data).catch(err => console.log(err)));
   };
-  return <p>hi</p>;
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [userinfo, setUserinfo] = useState(null);
+  const history = useHistory();
+
+  const isAuthenticated = () => {
+    setIsLogin(true);
+    setUserinfo('hello');
+  };
+
+  const handleResponseSuccess = () => {
+    isAuthenticated();
+    history.push('/');
+  };
+
+  useEffect(() => {
+    isAuthenticated();
+  }, []);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login
+              isLogin={isLogin}
+              handleResponseSuccess={handleResponseSuccess}
+            />
+          </Route>
+          <Route exact path="/signup">
+            <Signup isLogin={isLogin} />
+          </Route>
+          <Route exact path="/posts">
+            <Rollingpaper userinfo={userinfo} />
+          </Route>
+          <Route path="/">
+            {isLogin ? <Redirect to="/posts" /> : <Redirect to="/login" />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
