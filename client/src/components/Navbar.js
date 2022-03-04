2ac1406bd00f64fcb7c0f0336ad975136aacd7d0
Leekyeongmi <starry3ones@gmail.com>
2022-03-04 19:04:01 +0900

    #9 Design: 롤링페이퍼 기능과 디자인 결합

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-export default function Navbar() {
+export default function Navbar({ tt }) {
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-300 m-5 rounded-full">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <h1 className="font-bold text-3xl text-white leading-tight cursor-pointer">
-              To. Hello-Post
-              <br />
-            </h1>
+            <div className="flex">
+              <h1 className="font-bold text-3xl text-white leading-tight cursor-pointer">
+                To. Hello-Post
+              </h1>
+              <button
+                type="button"
+                className="text-md text-white text-6xl relative ml-5"
+              >
+                <span className="animate-[bounce_1s_ease-in-out_infinite] w-5 h-5 rounded-full absolute right-2 leading text-sm bg-red-500">
+                  {tt.length}
+                </span>
+                <svg
+                  width="10"
+                  height="10"
+                  fill="currentColor"
+                  viewBox="0 0 1792 1792"
+                  className="text-white h-10 w-10"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
+                </svg>
+              </button>
+            </div>
           </div>
+
           <div>
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
@@ -79,6 +99,7 @@ export default function Navbar() {
                   <span className="ml-2">MENU</span>
                 </a>
               </li>
+              <li className="nav-item"></li>
             </ul>
           </div>
         </div>
