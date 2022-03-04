64704ee194ed1be42ddf60fed3d3cf53d7da4798
River Ann <maragriver@gmail.com>
2022-03-04 20:50:18 +0900

    Edit: #42 라우터 위치, 메소드명 수정

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = express.Router();
-const postsRouter = require('./posts');
-const usersRouter = require('./users');
 
-router.use('/posts', postsRouter);
-router.use('/users', usersRouter);
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World from Server!');
+});
+
+// hello world 대신에 랜딩페이지/404 페이지 보여줄 수 있지 않을까
 
 module.exports = router;
