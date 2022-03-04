76aa20d617808bde5b0d233b04fff783c4206251
lindist12 <lindist12@gmail.com>
2022-03-04 11:16:26 +0900

    Add: #10 더미데이터로 메세지 조회 및 갯수 조회 추가

--- client/src/pages/Rollingpaper.js
+++ client/src/pages/Rollingpaper.js
@@ -1,1 +1,27 @@
-export default function Rollingpaper() {}
+import React, { useEffect, useState } from 'react';
+import List from '../components/List';
+import dummy from '../static/dummyData';
+
+const Rollingpaper = () => {
+  const [tt, tT] = useState(dummy);
+
+  useEffect(() => {
+    tT(dummy);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div className="total">
+        <span className="total-span">{'total: ' + tt.length}</span>
+      </div>
+      <ul>
+        {tt.map(a => {
+          return <List list={a} key={a.id} />;
+        })}
+        {/* <List /> */}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default Rollingpaper;
