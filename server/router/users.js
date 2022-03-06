42c0fe9e166c754f2c267b55988212c73f0b1db1
River Ann <maragriver@gmail.com>
2022-03-06 22:40:31 +0900

    Add: 로그인, 회원가입, 회원정보 수정, 회원 정보 조회 기능
    [추가로 작성할 부분]
    * users 테이블 쿼리: 회원 탈퇴, 회원정보 수정 -> users, posts 테이블 2개 사용
    * users 컨트롤러: 회원 탈퇴, 회원정보 수정
    * posts 컨트롤러 매소드 수정
    * posts 테이블 쿼리 작성: read, create, readAll, destroy
    ** 클라이언트가 사용하는 JSON과 서버가 반환하는 JSON 구조 맞추기

--- server/router/users.js
+++ server/router/users.js
@@ -6,24 +6,23 @@ const {
   signout,
   read,
   update,
+  destroy,
 } = require('../controller/users');
 
 // 회원가입
-// router.post('/signup', usersController.signup);
+router.post('/signup', signup);
 
 // 로그인
 router.post('/signin', signin);
 
 // 로그아웃
-// router.delete('/signout', usersController.signout);
+router.delete('/signout', signout);
 
 // 회원정보 조회
-// router.get('/:uid', usersController.read);
+router.get('/:uid', read);
 
-// 회원정보 수정, 회원탈퇴
-// router
-//   .route('/properties')
-//   .patch(usersController.update)
-//   .delete(usersController.delete);
+// PATCH /properties/update 회원정보 수정
+// DELETE /properties/destroy 회원탈퇴
+router.route('/properties').patch(update).delete(destroy);
 
 module.exports = router;
