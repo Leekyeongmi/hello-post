67fc0cddd2061a1d82435c38651d003274064daa
Leekyeongmi <starry3ones@gmail.com>
2022-03-06 20:17:36 +0900

    Feat: 사이드메뉴에서 로그아웃 기능 구현
    로그인된 유저가 사이드메뉴 열었을 때 로그아웃 처리 가능
    
    Resolves: #13

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Sidemenu from './Sidemenu';
 export default function Navbar({ tt, setShowSidemenu }) {
   return (
     <>
-      <nav className="top-5 relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-600 mx-5 rounded-full">
+      <nav className="top-3 relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-600 mx-5 rounded-full">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <div className="flex">
