a7254f0b8b6126d935386ffe75e5ab48b259e8de
River Ann <maragriver@gmail.com>
2022-03-08 20:44:34 +0900

    Add: userController.signin

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,9 +1,47 @@
+const { createAccessToken, isAuthorized } = require('../../utils/token');
+const { User, Post } = require('../../models');
+
 module.exports = {
   // 회원가입
   signup: (req, res) => {},
 
   // 로그인
-  signin: (req, res) => {},
+  signin: (req, res) => {
+    const decoded = isAuthorized(req);
+
+    if (!decoded) {
+      return res.status(401).json({ message: 'Unauthorized', data: null });
+    }
+    // decoded data
+    const { email, password } = req.body;
+
+    User.findOne({
+      where: { email, password },
+    }).then(theUser => {
+      if (!theUser) {
+        return res.status(403).json({ message: 'Token expired', data: null });
+      }
+      const { id, email, nickname, available, post_id } = theUser;
+
+      const title = Post.findOne({ where: { id: post_id } });
+
+      const accessToken = createAccessToken(theUser);
+
+      res.status(200).json({
+        message: 'ok',
+        data: {
+          accessToken,
+          uid: id,
+          email,
+          nickname,
+          available,
+          post_id,
+          title,
+        },
+      });
+      // 이후 posts/uid로 리디렉션
+    });
+  },
 
   // 로그아웃
   signout: (req, res) => {},
--- server/utils/token.js
+++ server/utils/token.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { sign, verify } = require('jsonwebtoken');
 
 module.exports = {
-  generateAccessToken: data => {
+  createAccessToken: data => {
     return sign(data, process.env.ACCESS_SECRET, { expiresIn: '15s' });
   },
 
