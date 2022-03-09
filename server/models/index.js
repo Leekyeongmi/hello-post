0c7cf143c71350d39f395699704f7561c61fec38
River Ann <maragriver@gmail.com>
2022-03-09 20:07:21 +0900

    Fix: rds 환경 변수 시퀄라이즈에게 전달

--- server/models/index.js
+++ server/models/index.js
@@ -12,12 +12,11 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    port: config.port,
+  });
 }
 
 fs.readdirSync(__dirname)
--- server/models/user.js
+++ server/models/user.js
@@ -15,7 +15,6 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      timestamps: false,
       modelName: 'User',
     }
   );
