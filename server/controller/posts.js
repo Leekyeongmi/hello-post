1427baec511ace7a10576fc7cada3b0cd616d429
River Ann <maragriver@gmail.com>
2022-03-09 12:40:32 +0900

    Update: 다회의 db 조회 필요한 로직에서 각각 try-catch로 분리

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -44,37 +44,57 @@ module.exports = {
     // 1) 로그인 안 한 방문자
     const token = isAuthorized(req);
 
+    const result = {};
+
     if (!token) {
       try {
         const thePost = await Post.findOne({ where: { user_id } });
         const { id, title } = thePost;
+
+        result.pid = id;
+        result.ptitle = title;
       } catch (e) {
         console.log('user_id로 post 조회 실패', e);
+        return res
+          .status(500)
+          .json({ message: '없는 사용자의 롤링페이퍼 입니다', data: null });
       }
       try {
-        const theMessage = Message.findAndCountAll({ where: { post_id: id } });
+        const theMessage = Message.findAndCountAll({
+          where: { post_id: result.pid },
+        });
         const { count, rows } = theMessage;
-      } catch (msgFindError) {
-        console.log('post_id로 message 조회 실패', msgFindError);
+
+        result.msgCount = count;
+        result.msgList = rows;
+      } catch (e) {
+        console.log('post_id로 message 조회 실패', e);
+        return res.status(500).json({
+          message: '해당 롤링페이퍼의 메시지를 불러오지 못했습니다.',
+          data: null,
+        });
+      }
+      try {
+        const { ptitle, msgCount, msgList } = result;
+        res.status(200).json({
+          message: 'ok',
+          data: {
+            title: ptitle,
+            total_message: msgCount,
+            messages: msgList,
+            // 👀 msgList는 객체 배열로, 클라에서 맵핑해서 보여주니 이대로 반환해도 되지 않을까 생각
+            // [{}, {}, {}]
+            // .map(msg => {
+            //   const { id, content, writer, created_at } = msg;
+            //   return { id, content, writer, created_at };
+            // }),
+          },
+        });
       } finally {
         return res
           .status(500)
           .json({ message: '해당 유저의 롤링페이퍼 조회 실패', data: null });
       }
-
-      res.status(200).json({
-        message: 'ok',
-        data: {
-          title,
-          total_message: count,
-          // rows는 객체 배열: [{}, {}, {}]
-          messages: rows.map(row => {
-            const { id, content, writer, created_at } = row;
-
-            return { id, content, writer, created_at };
-          }),
-        },
-      });
     }
     // 2) 로그인 된 유저 -> 사이드바 클릭 시 유저 정보까지 보여야 함
     else {
