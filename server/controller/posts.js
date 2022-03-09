11758f667fbe5b44071236a89a267c4e7833396f
River Ann <maragriver@gmail.com>
2022-03-09 11:56:32 +0900

    Fix: db 조회 에러 구분 처리 중

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -37,33 +37,43 @@ module.exports = {
 
   // 롤링페이퍼 조회
   // posts/:uid
-  read: (req, res) => {
+  read: async (req, res) => {
     const user_id = req.params.uid;
+    // console.log('😃req.params', req.params); // ✔uid로 넘어오는 것 확인
 
     // 1) 로그인 안 한 방문자
     const token = isAuthorized(req);
 
     if (!token) {
-      Post.findOne({ where: { user_id } }).then(thePost => {
+      try {
+        const thePost = await Post.findOne({ where: { user_id } });
         const { id, title } = thePost;
+      } catch (e) {
+        console.log('user_id로 post 조회 실패', e);
+      }
+      try {
+        const theMessage = Message.findAndCountAll({ where: { post_id: id } });
+        const { count, rows } = theMessage;
+      } catch (msgFindError) {
+        console.log('post_id로 message 조회 실패', msgFindError);
+      } finally {
+        return res
+          .status(500)
+          .json({ message: '해당 유저의 롤링페이퍼 조회 실패', data: null });
+      }
 
-        Message.findAndCountAll({ where: { post_id: id } }).then(theMessage => {
-          const { count, rows } = theMessage;
+      res.status(200).json({
+        message: 'ok',
+        data: {
+          title,
+          total_message: count,
+          // rows는 객체 배열: [{}, {}, {}]
+          messages: rows.map(row => {
+            const { id, content, writer, created_at } = row;
 
-          res.status(200).json({
-            message: 'ok',
-            data: {
-              title,
-              total_message: count,
-              // rows는 객체 배열: [{}, {}, {}]
-              messages: rows.map(row => {
-                const { id, content, writer, created_at } = row;
-
-                return { id, content, writer, created_at };
-              }),
-            },
-          });
-        });
+            return { id, content, writer, created_at };
+          }),
+        },
       });
     }
     // 2) 로그인 된 유저 -> 사이드바 클릭 시 유저 정보까지 보여야 함
